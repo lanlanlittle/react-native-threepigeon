5acbf5fa89add0f32e5bf7f51acc150dbce1b37a
zxx <405203958@qq.com>
2019-02-19 11:35:51 +0800

    ios 信鸽push

--- ios/RNXg.m
+++ ios/RNXg.m
@@ -17,6 +17,7 @@
         self.XG_ACCESS_KEY = [self getValueByKey:@"XG_ACCESS_KEY"];
         [[XGPush defaultManager] setEnableDebug:YES];
         [[XGPush defaultManager] startXGWithAppID:2200262432 appKey:@"I89WTUY132GJ" delegate:self];
+        [[XGPush defaultManager] reportXGNotificationInfo:NULL];
     }
     return self;
 }
@@ -31,6 +32,66 @@
     }
     
     return @"";
+}
+
+#pragma mark - XGPushDelegate
+- (void)xgPushDidFinishStart:(BOOL)isSuccess error:(NSError *)error {
+    NSLog(@"%s, result %@, error %@", __FUNCTION__, isSuccess?@"OK":@"NO", error);
+}
+
+- (void)xgPushDidFinishStop:(BOOL)isSuccess error:(NSError *)error {
+    NSLog(@"%s, result %@, error %@", __FUNCTION__, isSuccess?@"OK":@"NO", error);
+}
+
+- (void)xgPushDidRegisteredDeviceToken:(NSString *)deviceToken error:(NSError *)error {
+    NSLog(@"%s, result %@, error %@", __FUNCTION__, error?@"NO":@"OK", error);
+}
+
+// iOS 10 新增 API
+// iOS 10 会走新 API, iOS 10 以前会走到老 API
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+// App 用户点击通知
+// App 用户选择通知中的行为
+// App 用户在通知中心清除消息
+// 无论本地推送还是远程推送都会走这个回调
+- (void)xgPushUserNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler {
+    NSLog(@"[XGDemo] click notification");
+    if ([response.actionIdentifier isEqualToString:@"xgaction001"]) {
+        NSLog(@"click from Action1");
+    } else if ([response.actionIdentifier isEqualToString:@"xgaction002"]) {
+        NSLog(@"click from Action2");
+    }
+    
+    [[XGPush defaultManager] reportXGNotificationResponse:response];
+    
+    completionHandler();
+}
+
+// App 在前台弹通知需要调用这个接口
+//- (void)xgPushUserNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+//    [[XGPush defaultManager] reportXGNotificationInfo:notification.request.content.userInfo];
+//    completionHandler(UNNotificationPresentationOptionBadge | UNNotificationPresentationOptionSound | UNNotificationPresentationOptionAlert);
+//}
+#endif
+
+/**
+ 统一收到通知消息的回调
+ @param notification 消息对象
+ @param completionHandler 完成回调
+ @note SDK 3.2.0+
+ */
+- (void)xgPushDidReceiveRemoteNotification:(id)notification withCompletionHandler:(void (^)(NSUInteger))completionHandler {
+    if ([notification isKindOfClass:[NSDictionary class]]) {
+        [[XGPush defaultManager] reportXGNotificationInfo:(NSDictionary *)notification];
+        completionHandler(UIBackgroundFetchResultNewData);
+    } else if ([notification isKindOfClass:[UNNotification class]]) {
+        [[XGPush defaultManager] reportXGNotificationInfo:((UNNotification *)notification).request.content.userInfo];
+        completionHandler(UNNotificationPresentationOptionBadge | UNNotificationPresentationOptionSound | UNNotificationPresentationOptionAlert);
+    }
+}
+
+- (void)xgPushDidSetBadge:(BOOL)isSuccess error:(NSError *)error {
+    NSLog(@"%s, result %@, error %@", __FUNCTION__, error?@"NO":@"OK", error);
 }
 
 RCT_EXPORT_MODULE()
